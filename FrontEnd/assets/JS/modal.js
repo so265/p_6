597f170f2aceb4d7a9c6f15e687ee24a5d5916fb
so265 <undia42@gmail.com>
2023-06-25 19:06:15 +0200

    suppression image modale

--- FrontEnd/assets/JS/modal.js
+++ FrontEnd/assets/JS/modal.js
@@ -47,6 +47,8 @@ function displayImagesInModal() {
         const imageContainer = document.createElement("div");
         imageContainer.classList.add("modal-image-container");
 
+        imageContainer.dataset.id = work.id//dataset est une propriété des éléments du DOM
+
         const image = document.createElement("img");
         image.src = work.imageUrl;
         image.alt = "Image du projet";
@@ -143,8 +145,8 @@ arrowLeft.addEventListener("click", goBackToFirstModal); //au click sur la fléc
 galleryImagesInModal.addEventListener("click", (e) => {  //"galleryImagesInModal" correspond à ma gallery d'image
   if (e.target.classList.contains("delete-icon")) { //delete-icon ds modal-css, ligne 105, c'est ma corbeille//"e.target", je determine l'élément réellement cliqué, je vérifie que cet élement posséde la classe"delete-icon"
     const imageContainer = e.target.parentElement; //Avec"parentElement", je remonte jusqu'à l'élément conteneur de l'image qui est "imageContainer" qui contient l'image + corbeille
-    const imageId = imageContainer.getAttribute("data-id"); //j' extrais l'ID de l'image à partir de l'attribut data-id de imageContainer."data-id" représente l'identifiant de l'image correspondante
-//Lorsque je  clique sur la corbeille j'utilise imageContainer.getAttribute("data-id") pour récupérer la valeur de l'attribut data-id, qui représente l'identifiant de l'image correspondante.
+    //const imageId = imageContainer.dataset.id; // je mets en commentaire pour pas vraiment supprimer l'image/j' extrais l'ID de l'image à partir de l'attribut data-id de imageContainer."data-id" représente l'identifiant de l'image correspondante
+//Lorsque je  clique sur la corbeille j'utilise imageContainer.dataset.id pour récupérer la valeur de l'attribut data-id, qui représente l'identifiant de l'image correspondante.
 
     fetch(`http://localhost:5678/api/works/${imageId}`, {  // L'URL de la requête est construite en utilisant l'ID de l'image, "imageId"=variable L137, permet de spécifier quelle image doit être suprimée
       method: "DELETE", //Je fais 1 requéte delete à l'API en utilisant 'fetch'
